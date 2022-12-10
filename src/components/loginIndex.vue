8edbb55b6c581df0cebb6096d1f2493794631e6c
曹鸿杰 <727980087@qq.com>
2022-12-10 18:01:05 +0800

    less配置

---
+++ src/components/loginIndex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="loginWrap">
+    {{ name }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const name = ref('dawdwa')
+</script>
+
+<style scoped lang="less">
+.loginWrap {
+  color: @grey;
+  width: 100%;
+  height: 100vh;
+  background: url('@/assets/images/login/loginBg.jpg') no-repeat;
+  background-size: cover;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
+const {
+  defineConfig
+} = require("@vue/cli-service");
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [
+        // path.join(__dirname, "./src/assets/styles/common.less"),
+        path.join(__dirname, "./src/assets/styles/variable.less"),
+      ],
+    },
+  },
+
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", path.resolve("./src"))
+  },
+
+});
